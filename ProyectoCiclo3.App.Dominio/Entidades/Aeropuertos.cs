2d52bfb408b43887b4aaa6e50775f0367f48feca
normalelele <juniorgamer096@gmail.com>
2022-09-08 20:49:12 -0500

    fourth commit

--- ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aeropuertos.cs
@@ -2,6 +2,7 @@ using System;
 namespace ProyectoCiclo3.App.Dominio{
     public class Aeropuertos{
         public int id { get; set; }
+        public string nombre { get; set; }
         public string ciudad { get; set; }
         public string pais { get; set; }
         public string coord_x { get; set; }
--- ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
+++ ProyectoCiclo3.App.Dominio/Entidades/Aviones.cs
@@ -2,10 +2,10 @@ using System;
 namespace ProyectoCiclo3.App.Dominio{
     public class Aviones{
         public int id { get; set; }
+        public int modelo { get; set; }
         public string marca { get; set; }
-        public string modelo { get; set; }
         public int numero_asientos { get; set; }
-        public int numero_baños { get; set; }
-        public float capacidad_maxima { get; set; }
+        public int numero_banos { get; set; }
+        public int capacidad { get; set; }
     }
 }
